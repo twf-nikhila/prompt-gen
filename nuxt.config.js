4e055bd5fb4a450b3a43e418838eaf64175620de
Nikhil Agrawal <nikhila@thewebfosters.com>
2022-09-23 21:03:28 +0530

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'promp-generator',
+    title: 'Stable Diffusion Prompt Generator',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
